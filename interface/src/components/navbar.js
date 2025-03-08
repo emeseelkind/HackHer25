43ac3a3d3ea304bdde14225d06a56cc092fe4d3f
Shrinidhi Krishnan <98567631+kshrin2002@users.noreply.github.com>
2025-03-08 11:49:03 -0500

    Navbar

---
+++ interface/src/components/navbar.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./navbar.css"; // Import the CSS file
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <div className="circle">NB</div>
+        <h1 className="navbar-title">NutriByte</h1>
+      </div>
+
+      <div className="nav-links">
+        {/* Personal Dropdown */}
+        <div className="dropdown">
+          <button>Personal</button>
+          <div className="dropdown-menu">
+            <div>Intake</div>
+            <div>Goals</div>
+          </div>
+        </div>
+
+        {/* Community Dropdown */}
+        <div className="dropdown">
+          <button>Community</button>
+          <div className="dropdown-menu">
+            <div>Connect</div>
+            <div>Boost</div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
