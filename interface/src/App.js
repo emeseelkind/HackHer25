bad07273696d81d59bbc8fb59ca6d28b8a0855d0
Shrinidhi Krishnan <98567631+kshrin2002@users.noreply.github.com>
2025-03-08 13:28:25 -0500

    more changes

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 import Navbar from "./components/navbar"; // Capitalize first letter
 import "./App.css"; // Import the CSS file
 import Connect from "./components/Connect"; // Import the Connect component
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar"; // Capitalize first letter
+import "./App.css"; // Import the CSS file
 import SignUp from "./components/SignUp"; // Capitalize first letter
+
 
 function App() {
   return (
@@ -13,6 +19,7 @@ function App() {
         <Route path = "/signup" element = {<SignUp />} />
       </Routes>
     </Router>
+
   );
 
 }
