3a60b2309c5b2cd2b4a54f9eb91487816d2b6e6c
Emese Elkind <emese.elkind@gmail.com>
2025-03-08 13:09:24 -0500

    Your commit message here

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import Navbar from "./components/navbar"; // Capitalize first letter
 import "./App.css"; // Import the CSS file
+import Connect from "./components/Connect"; // Import the Connect component
 
 function App() {
+  const [activeSection, setActiveSection] = useState(null);
+  const [activeSubSection, setActiveSubSection] = useState(null);
+
+  const handleNavigation = (section, subSection) => {
+    setActiveSection(section);
+    setActiveSubSection(subSection);
+  };
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar onNavigate={handleNavigation} />
+      
+      {activeSection === "community" && activeSubSection === "connect" && <Connect />}
     </div>
   );
-
 }
 
 export default App;
